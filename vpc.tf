30db8b9d2f9db3a7e5888a7c7cf89e9eb0d1fa34
kevincloud <40193276+kevincloud@users.noreply.github.com>
2020-01-06 23:33:17 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -22,4 +22,13 @@ module "vpc" {
   public_subnet_tags = {
     Name = "overridden-name-public"
   }
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name"
+  }  
 }
